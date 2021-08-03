2d692c4030a9162823a69b70b55ef636c2563902
mpfullstack <info@marcperez.cat>
2021-08-03 08:32:53 +0200

    Added redux support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import '../styles/globals.css'
 import App, { AppContext } from 'next/app';
 import type { AppProps } from 'next/app'
+import { wrapper } from '../src/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
+
   // calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext);
 
   return { ...appProps }
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
---
+++ src/store.ts
@@ -0,0 +1,46 @@
+import { Reducer, Action, createStore, combineReducers, applyMiddleware, AnyAction } from 'redux';
+import { useDispatch as _useDispatch } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { HYDRATE, MakeStore, createWrapper } from 'next-redux-wrapper';
+
+export const initialState: GlobalState = {
+  identity: {}
+};
+
+interface GlobalState {
+  identity: object;
+};
+
+export type ActionWithPayload<T, P> = Action<T> & { payload: P };
+
+export type ReduxAction =
+  | Action<AnyAction>
+  | ActionWithPayload<typeof HYDRATE, GlobalState>;
+
+const reducer: Reducer<GlobalState, ReduxAction> = (state = initialState, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      debugger;
+      const nextState = {
+        ...state, // use previous state
+        ...action.payload.common, // apply delta from hydration
+      };
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export type RootState = ReturnType<typeof reducer>;
+export type Store = ReturnType<typeof configureStore>;
+
+const rootReducer = combineReducers({
+  common: reducer
+});
+
+export const configureStore = () => createStore(rootReducer, composeWithDevTools());
+const makeStore: MakeStore<Store> = configureStore;
+
+export const wrapper = createWrapper(makeStore, { debug: false });
+
+export default makeStore;
